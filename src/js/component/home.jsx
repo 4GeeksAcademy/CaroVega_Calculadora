2babb53bebb12fde0a38d0ea4ff5b1f6d40972f5
Adriana Carolina Vega Ahumada <carolinavegaahumada@gmail.com>
2023-09-09 11:54:41 +0000

    keyboard calculator

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import Pantalla from "./component/pantalla.jsx";
+import Number from "./number";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1>contenedor</h1>
+		<div className="calculator">
+			<Number/>
 		</div>
+			
 	);
 };
 
---
+++ src/js/component/number.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Number = () => {
+	return (
+		<div className="keyboard">
+            <div className ="rowcal">
+                <div className="button operation">C</div>
+                <div className="button operation">+-</div>
+                <div className="button operation">%</div>
+                <div className="button operation">-</div>
+            </div>
+            <div className ="rowcal">
+			    <div className="button text-center"><p>7</p></div>
+                <div className="button">8</div>
+                <div className="button">9</div>
+                <div className="button operation">X</div>
+            </div>
+            <div className ="rowcal">
+                <div className="button">4</div>
+                <div className="button">5</div>
+                <div className="button">6</div>
+                <div className="button operation">-</div>
+            </div>
+            <div className ="rowcal">
+                <div className="button">1</div>
+                <div className="button">2</div>
+                <div className="button">3</div>
+                <div className="button operation">+</div>
+            </div>
+            <div className ="rowcal">
+                <div className="button">0</div>
+                <div className="button operation">.</div>
+                <div className="button operationequal">=</div>
+            </div>
+        </div>
+		
+	);
+};
+
+export default Number;
